e63009aba9a37a4b6ac054a91f23eba21799f9de
ckt522 <34671064+chankin522@users.noreply.github.com>
2024-12-25 14:13:53 +0000

    added smtp

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default () => ({
+    email: {
+        config: {
+          provider: 'strapi-provider-email-brevo',
+          providerOptions: {
+            apiKey: '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',
+          },
+          settings: {
+            defaultSenderEmail: 'tak@add-values.com',
+            defaultSenderName: 'Admin',
+            defaultReplyTo: 'tak@add-values.com',
+          },
+        },
+      }
+});
