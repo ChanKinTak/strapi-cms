716350cecb19758f9e8cafc52ca9c2899a948f08
ckt522 <34671064+chankin522@users.noreply.github.com>
2024-12-25 14:38:08 +0000

    v3

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,3 @@
 export default () => ({
-    email: {
-        config: {
-          provider: 'strapi-provider-email-brevo',
-          providerOptions: {
-            apiKey: env("BREVO_API_KEY"),
-          },
-          settings: {
-            defaultFrom: env("EMAIL_DEFAULT_FROM"), 
-            defaultSenderName: 'Admin',
-            defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"), 
-          },
-        },
-      }
+    
 });
