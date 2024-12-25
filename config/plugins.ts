eaf8e674306cfd78f6b252ca1570beb2a059d415
ckt522 <34671064+chankin522@users.noreply.github.com>
2024-12-25 15:58:49 +0000

    added email plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default ({ env }) => ({});
+export default {
+  email: {
+    config: {
+      provider: '@strapi/provider-email-sendmail',
+      providerOptions: {
+        smtp: {
+          host: process.env.SMTP_HOST,
+          port: Number(process.env.SMTP_PORT),
+          secure: process.env.SMTP_SECURE === 'true',
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+          }
+        }
+      },
+      settings: {
+        defaultFrom: process.env.SMTP_FROM,
+        defaultReplyTo: process.env.SMTP_REPLY_TO
+      }
+    }
+  }
+} as const;
