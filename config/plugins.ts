266ff9dc447c32df30b2f99e97d5f7e99cb0afb9
ckt522 <34671064+chankin522@users.noreply.github.com>
2024-12-25 15:43:52 +0000

    uninstall smtp

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,1 @@
-export default ({ env }) => ({
-  email: {
-    config: {
-      provider: "brevo", // 使用 brevo 提供商
-      providerOptions: {
-        apiKey: env("BREVO_API_KEY"), // 从环境变量中获取 API 密钥
-      },
-      settings: {
-        defaultFrom: env("EMAIL_DEFAULT_FROM"), // 默认发件人
-        defaultSenderName: 'Admin',
-        defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"), // 默认回复地址
-      },
-    },
-  },
-});
+export default ({ env }) => ({});
