3aab24eea56bb8a9dfd02717ecadeb9282c87be2
ckt522 <34671064+chankin522@users.noreply.github.com>
2024-12-25 14:19:53 +0000

    updated smtp

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,7 +3,7 @@ export default () => ({
         config: {
           provider: 'strapi-provider-email-brevo',
           providerOptions: {
-            apiKey: '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',
+            apiKey: process.env.SENDINBLUE_API_KEY,
           },
           settings: {
             defaultSenderEmail: 'tak@add-values.com',
