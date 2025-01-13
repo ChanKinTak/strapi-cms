741990939c30255af2a8db9c357103959a656f0f
ckt522 <34671064+chankin522@users.noreply.github.com>
2025-01-13 18:59:47 +0000

    updated email

--- src/api/registration/content-types/registration/lifecycles.ts
+++ src/api/registration/content-types/registration/lifecycles.ts
@@ -4,7 +4,7 @@ export default {
 
         try{
             await strapi.plugins['email'].services.email.send({
-                to: '${email}',     // 必須有效的郵箱地址
+                to: '${result.email}',     // 必須有效的郵箱地址
                 from: process.env.SMTP_FROM, // 必須是在 SendGrid 已驗證的域名
                 subject: '成功登記布拉格海外會議2025 Successfully registered for P2025 Prague Convention',         // 不能為空
                 text: '已成功登記 Successfully registered'              // text 或 html 至少要有一個
