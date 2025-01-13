e047fde55071286a2ef153096c680847ca2c407b
ckt522 <34671064+chankin522@users.noreply.github.com>
2025-01-13 18:21:04 +0000

    updated email

--- src/api/faq/content-types/faq/lifecycles.ts
+++ src/api/faq/content-types/faq/lifecycles.ts
@@ -5,7 +5,7 @@ export default {
         try{
             await strapi.plugins['email'].services.email.send({
                 to: 'tak@add-values.com',     // 必須有效的郵箱地址
-                from: env('SMTP_FROM'), // 必須是在 SendGrid 已驗證的域名
+                from: process.env.SMTP_FROM, // 必須是在 SendGrid 已驗證的域名
                 subject: 'Test Subject',         // 不能為空
                 text: 'Hello world'              // text 或 html 至少要有一個
               });
