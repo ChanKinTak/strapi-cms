0d4281da03edd9fd611f12dc58bfe620f3ecb1d7
ckt522 <34671064+chankin522@users.noreply.github.com>
2025-01-15 15:05:24 +0000

    updated user

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,40 +1,4 @@
-/*export default ({ env }) => ({
-  email: {
-    config: {
-      provider: '@strapi/provider-email-sendmail',
-      providerOptions: {
-        smtp: {
-          host: env('SMTP_HOST'),
-          port: env('SMTP_PORT'), 
-          secure: false,
-          auth: {
-            user: env('SMTP_USERNAME'),
-            pass: env('SMTP_PASSWORD')
-          },
-          requireTLS: true,
-          logger: true  // 添加这行来获取详细日志
-        }
-      },
-      settings: {
-        defaultFrom: env('SMTP_FROM'),
-        defaultReplyTo: env('SMTP_TO')
-      }
-    }
-  }
-});*/
-
-
 export default ({ env }) => ({
-  'users-permissions': {
-    config: {
-      jwt: {
-        expiresIn: '7d',
-      },
-      forgotPassword: {
-        tokenLength: 4
-      }
-    }
-  },
   email: {
     config: {
       provider: 'sendgrid',
