c506a089ea04b8cb4ac2416af5385f211daad6bc
ckt522 <34671064+chankin522@users.noreply.github.com>
2025-03-16 16:39:21 +0000

    new 11

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1514,7 +1514,12 @@ export interface PluginUsersPermissionsUser
     full_name: Schema.Attribute.String;
     Group: Schema.Attribute.Enumeration<['Group01', 'Group02', 'Group03']>;
     hotel_arrangement: Schema.Attribute.Enumeration<
-      ['\u2060Hilton Prague', 'Prague Marriott Hotel', 'Hilton Prague Old Town']
+      [
+        '\u2060Hilton Prague',
+        'Prague Marriott Hotel',
+        'Hilton Prague Old Town',
+        'TBC',
+      ]
     >;
     isFirstLogin: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<true>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
